271536e8962f06694cde1cbfc3d57489f505b7cf
dimitar <90458809+dimops@users.noreply.github.com>
2022-12-14 23:48:22 +0200

    Built-in-functions with library database comleted

--- built-in-functions/built-in-functions-basic.sql
+++ built-in-functions/built-in-functions-basic.sql
@@ -1,1 +1,16 @@
 -- book_library --
+
+SELECT title FROM `books`
+WHERE SUBSTR(title, 1, 3) = 'The';
+
+SELECT REPLACE(title, 'The', '***') AS 'Title' FROM `books`
+WHERE SUBSTR(title, 1, 3) = 'The';
+
+SELECT ROUND(SUM(cost), 2) AS 'Cost' FROM `books`;
+
+SELECT CONCAT_WS(' ', first_name, last_name) AS 'Full Name', TIMESTAMPDIFF(DAY, born, died) AS 'Days Lived'
+FROM `authors`;
+
+SELECT title FROM `books`
+WHERE  title LIKE '%Harry Potter%'
+ORDER BY id;
