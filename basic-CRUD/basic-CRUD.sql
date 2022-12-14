453f96d24a50fa9e279700575d64cbb0818cb8db
dimitar <90458809+dimops@users.noreply.github.com>
2022-12-14 21:22:56 +0200

    Deletion of data

--- basic-CRUD/basic-CRUD.sql
+++ basic-CRUD/basic-CRUD.sql
@@ -8,3 +8,22 @@ ORDER BY id;
 UPDATE employees
     SET salary = salary + 100
     WHERE job_title = 'Manager';
+
+
+UPDATE employees
+    SET salary = salary + 100
+    WHERE job_title = 'Manager';
+
+SELECT * FROM employees
+ORDER BY salary DESC
+LIMIT 1;
+
+SELECT * FROM employees
+WHERE department_id = 4
+    AND salary > 1000
+ORDER BY id;
+
+DELETE FROM `employees`
+WHERE `department_id` = 1
+OR `department_id` = 2;
+SELECT * from employees;
