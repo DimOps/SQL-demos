1cb29ad93bb64487119051fdc5b4f009fd1e80c4
dimitar <90458809+dimops@users.noreply.github.com>
2022-12-14 17:16:23 +0200

    Updated value from table

--- basic-CRUD/basic-CRUD.sql
+++ basic-CRUD/basic-CRUD.sql
@@ -4,3 +4,7 @@ ORDER BY id;
 SELECT id, CONCAT_WS(' ', first_name, last_name) AS 'full_name', job_title, salary FROM employees
 WHERE salary > 1000
 ORDER BY id;
+
+UPDATE employees
+    SET salary = salary + 100
+    WHERE job_title = 'Manager';
