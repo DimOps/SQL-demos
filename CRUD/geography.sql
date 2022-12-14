59f45922add70a144fc19db9927a384bb7b57e21
dimitar <90458809+dimops@users.noreply.github.com>
2022-12-14 23:02:14 +0200

    Geography database CRUD operations comleted

---
+++ CRUD/geography.sql
@@ -0,0 +1,11 @@
+SELECT peak_name FROM `peaks`
+ORDER BY peak_name;
+
+SELECT country_name, population FROM `countries`
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name;
+
+SELECT country_name, country_code, currency_code,
+IF(currency_code = 'EUR', 'Euro', 'Not Euro') AS 'currency'
+FROM `countries`
+ORDER BY country_name;
